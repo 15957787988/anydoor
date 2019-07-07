edb68251b534a1464372aebe49ba67581fdac129
逍云遥 <you@example.com>
2019-07-07 15:27:57 +0800

    'test'

---
+++ src/app.js
@@ -0,0 +1,26 @@
+// const http = require("http")
+// const chalk = require('chalk')
+// const conf = require('./config/defultConfig.js')
+
+// const server = http.createServer((req,res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.write('<html>')
+//   res.write('<body>')
+//   res.write('hello world asddsas!')
+//   res.write('</body>')
+//   res.write('</html>')
+//   res.end()
+
+// })
+
+// server.listen(conf.prot, conf.hostname, () => {
+//   const addr = `http://${conf.hostname}:${conf.prot}`
+//   console.info(addr)
+// })
+
+console.log('1111');
+
+/*eslint-disable */
+console.log("1111")
+/*eslint-enable*/
